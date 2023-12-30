e365156507ca98aeb07b02e86486721ce9fc26e0
achiar99 <34912231+achiar99@users.noreply.github.com>
2023-12-30 18:36:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import boto3
 import datetime
 from config import profile, region, event, sfn, resources, mocks
+from parse import parse_states
 
 
 class bcolors:
@@ -17,92 +18,6 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-
-
-
-class State:
-    def __init__(self, name, type) -> None:
-        self.name = name
-        self.type = type
-        self.next = ''
-        self.end = False
-        self.debug = False
-        self.mock = False
-
-
-class Task(State):
-    def __init__(self, name, type, resource, result_path, parameters) -> None:
-        super().__init__(name, type)
-        self.resource = resource
-        self.result_path = result_path
-        self.parameters = parameters
-
-
-class Choice(State):
-    def __init__(self, name, type, choices, default) -> None:
-        super().__init__(name, type)
-        self.choices = choices
-        self.default = default
-
-
-class Pass(State):
-    def __init__(self, name, type) -> None:
-        super().__init__(name, type)
-        self.result = None
-        self.result_path = None
-
-
-class Success(State):
-    def __init__(self, name, type) -> None:
-        super().__init__(name, type)
-
-
-class Fail(State):
-    def __init__(self, name, type) -> None:
-        super().__init__(name, type)
-
-
-class Map(State):
-    def __init__(self, name, type,) -> None:
-        super().__init__(name, type)
-        self.item_path = None
-        self.item_selector = None
-        self.item_processor = None
-        self.catch = False
-
-
-def parse_states(definition):
-    states = {}
-    for name, state in definition.get('States').items():
-        state_type = state.get('Type')
-        if state_type == 'Task':
-            states[name] = Task(name=name, type=state_type, resource=state.get('Resource'), result_path=state.get('ResultPath'), parameters=state.get('Parameters'))
-            states[name].next = state.get('Next', '')
-            states[name].end = state.get('End', False)
-            states[name].debug = state.get('Debug', False)
-            states[name].mock = state.get('Mock', False)
-        if state_type == 'Choice':
-            states[name] = Choice(name=name, type=state_type, choices=state.get('Choices'), default=state.get('Default'))
-        if state_type == 'Pass':
-            states[name] = Pass(name=name, type=state_type)
-            states[name].result = state.get('Result')
-            states[name].result_path = state.get('ResultPath')
-            states[name].next = state.get('Next', '')
-        if state_type == 'Fail':
-            states[name] = Fail(name=name, type=state_type)
-            states[name].end = True
-        if state_type == 'Succeed':
-            states[name] = Success(name=name, type=state_type)
-            states[name].end = True
-        if state_type == 'Map':
-            states[name] = Map(name=name, type=state_type)
-            states[name].item_path = state.get('ItemsPath')
-            states[name].item_selector = state.get('ItemSelector')
-            states[name].item_processor = state.get('ItemProcessor')
-            states[name].catch = state.get('Catch')
-            states[name].next = state.get('Next', '')
-    return states
-
 
 
 def fix_param(param, data):
